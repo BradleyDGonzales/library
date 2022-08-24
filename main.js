bfc446d0978ee375a7a7e83c2fb61e0eb00aeb3e
Bradley <gonzales.bradleyd@gmail.com>
2022-08-24 00:17:39 -0400

    Add library logic

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ TODO: Figured out how to push objects into an array, so at this point just need 
 TODO 08/22: gathered input from data, DOM manipulation working, made a small popup form (nothing special), as of rn just need to
             actually display the form data onto the webpage as well as making everything required (check for `` or null or undefined values in input(?));
 
+
+TODO 08/23: added a more pleasing looking webpage, figured out how to append child to last value of card-row div but still need to actually display the new book onto the webpage. also still
+            need to toggle read status
 */
 
 
 const newBookButton = document.getElementById(`btnAddForm`);
+const myBooksDiv = document.querySelector(`.myBooks`)
+console.log(myBooksDiv);
 let myLibrary = [];
 let clickCount = 0;
+let called = false;
+
 
 let title;
 let author;
@@ -30,13 +37,14 @@ function Book(title, author, pageCount, read) {
 //function that makes new object and pushes it to myLibrary[]
 function addBookToLibrary(title, author, pageCount, read) {
 
-  myLibrary.push({title: `${title}`,
-  author: `${author}`,
-  pageCount:`${pageCount}`,
-  read: `${read}`});
-  
+  myLibrary.push({
+    title: `${title}`,
+    author: `${author}`,
+    pageCount: `${pageCount}`,
+    read: `${read}`
+  });
+
   const newBook = new Book(title, author, pageCount, read)
-  console.log(newBook);
 
 }
 //dynamic form
@@ -48,56 +56,53 @@ function makeForm() {
     return;
   }
 
-  const doneButton = document.createElement(`button`);
-  doneButton.setAttribute(`type`,`button`)
-  doneButton.innerText = `Done`;
 
 
   const newForm = document.createElement(`form`);
-  newForm.setAttribute(`id`,`bookForm`);
+  newForm.setAttribute(`id`, `bookForm`);
 
   const labelBookName = document.createElement(`label`);
-  labelBookName.setAttribute(`for`,`title`);
+  labelBookName.setAttribute(`for`, `title`);
   labelBookName.innerText = `Name of book: `
 
   const bookName = document.createElement(`input`);
-  bookName.setAttribute(`type`,`text`);
-  bookName.setAttribute(`name`,`title`);
-  bookName.setAttribute(`id`,`title`);
+  bookName.setAttribute(`type`, `text`);
+  bookName.setAttribute(`name`, `title`);
+  bookName.setAttribute(`id`, `title`);
+  bookName.required = true;
 
   const labelAuthorName = document.createElement(`label`);
-  labelAuthorName.setAttribute(`for`,`author`);
+  labelAuthorName.setAttribute(`for`, `author`);
   labelAuthorName.innerText = `Author: `;
 
   const authorName = document.createElement(`input`);
-  authorName.setAttribute(`type`,`text`);
-  authorName.setAttribute(`name`,`author`);
-  authorName.setAttribute(`id`,`author`);
+  authorName.setAttribute(`type`, `text`);
+  authorName.setAttribute(`name`, `author`);
+  authorName.setAttribute(`id`, `author`);
+  authorName.required = true;
 
   const labelPageCount = document.createElement(`label`);
-  labelPageCount.setAttribute(`for`,`pagecount`);
+  labelPageCount.setAttribute(`for`, `pagecount`);
   labelPageCount.innerText = `Page Count: `;
 
   const pageCountEl = document.createElement(`input`);
-  pageCountEl.setAttribute(`type`,`number`);
-  pageCountEl.setAttribute(`name`,`pagecount`);
-  pageCountEl.setAttribute(`id`,`pagecount`);
-  pageCountEl.setAttribute(`required`,``);
+  pageCountEl.setAttribute(`type`, `number`);
+  pageCountEl.setAttribute(`name`, `pagecount`);
+  pageCountEl.setAttribute(`id`, `pagecount`);
+  pageCountEl.required = true;
 
   const labelReadBook = document.createElement(`label`);
-  labelReadBook.setAttribute(`for`,`read`);
+  labelReadBook.setAttribute(`for`, `read`);
   labelReadBook.innerText = `Read? `;
 
   const readBook = document.createElement(`input`);
-  readBook.setAttribute(`type`,`checkbox`);
-  readBook.setAttribute(`name`,`read`);
-  readBook.setAttribute(`id`,`read`);
-  readBook.setAttribute(`required`,``)
+  readBook.setAttribute(`type`, `checkbox`);
+  readBook.setAttribute(`name`, `read`);
+  readBook.setAttribute(`id`, `read`);
+
   const submitButton = document.createElement(`input`);
-  submitButton.setAttribute(`type`,`submit`);
-  submitButton.setAttribute(`onclick`,`closeForm()`)
-
-
+  submitButton.setAttribute(`type`, `submit`);
+  submitButton.setAttribute(`onclick`, `closeForm()`)
 
 
   newForm.appendChild(labelBookName);
@@ -109,9 +114,8 @@ function makeForm() {
   newForm.appendChild(labelReadBook);
   newForm.appendChild(readBook);
   newForm.appendChild(submitButton);
-  
+
   //document.getElementsByTagName("div")[0].style.display = `block`
-  console.log(typeof document.forms[0])
   if (typeof document.forms[0] !== `undefined`) return;
   document.getElementById(`popup`).appendChild(newForm);
 
@@ -120,25 +124,47 @@ function makeForm() {
   const myForm = document.querySelectorAll(`form`);
   const submitInput = myForm[0].querySelector(`input[type="submit"]`);
 
-  submitInput.addEventListener(`click`, function(e) {
+  submitInput.addEventListener(`click`, function (e) {
     e.preventDefault();
-    
-    
+
+
+    const inputBookName = document.getElementById(`title`);
+    const inputAuthorName = document.getElementById(`author`);
+    const inputPageCount = document.getElementById(`pagecount`);
+    const inputReadBook = document.getElementById(`read`);
+
+
     title = document.querySelector(`#title`).value
     author = document.querySelector(`#author`).value
     pageCount = document.querySelector(`#pagecount`).value
-    read = document.querySelector(`#read`).value
-    
+
+    if (readBook.checked) {
+      read = `Read`;
+    }
+    else {
+      read = `Not yet read`
+    }
+
+    if (!(inputBookName.checkValidity())) {
+      inputBookName.reportValidity();
+      return;
+    }
+    if (!(inputAuthorName.checkValidity())) {
+      inputAuthorName.reportValidity();
+      return;
+    }
+    if (!(inputPageCount.checkValidity())) {
+      inputPageCount.reportValidity();
+      return;
+    }
     let userBook = new Book(title, author, pageCount, read)
     addBookToLibrary(title, author, pageCount, read);
-    console.log(title, author, pageCount, read);
     console.table(userBook);
-    
-    console.log(myLibrary)
+
     clickCount = 0;
     document.getElementById("bookForm").reset();
     closeForm();
-    
+    addBookToWebpage();
 
 
 
@@ -146,12 +172,44 @@ function makeForm() {
 
 }
 function closeForm() {
-  popup.classList.remove(`open-popup`);
+  if ((title === `` || title === undefined) || (author === `` || author === undefined) || (pageCount === `` || pageCount === undefined) || (read === `` || read === undefined)) {
+    return;
+  }
+  else {
+    title = ``;
+    author = ``;
+    pageCount = ``;
+    read = ``;
+    popup.classList.remove(`open-popup`);
+
+  }
+}
+
+function addBookToWebpage() {
+
+  //creates new H2 Element
+  const newH2Element = document.createElement(`h2`);
+  newH2Element.classList.add(`labelvalue`);
+  newH2Element.setAttribute(`id`,`titlevalue1`)
+
+  const cardRow = document.getElementById(`card-row`)
+  cardRow.appendChild(newH2Element)
+
+  //const lastElement = document.getElementById(`titlevalue`);
+  //lastElement.innerText = `Testing this`
+
 }
 //loops through myLibrary[]
 function looper(array) {
-  for (let i = 0; i < array.length;i++) {
-    console.log(array[i]);
+  for (let i = 0; i < array.length; i++) {
+    title = array[i].title;
+    author = array[i].author;
+    pageCount = array[i].pageCount;
+    read = array[i].read;
   }
+  console.log(array);
 }
-looper(myLibrary);
+//looper(myLibrary);
+
+
+
