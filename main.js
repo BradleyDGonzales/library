7a0f98bc677088656f5b504be04ba98fade3205e
Bradley <gonzales.bradleyd@gmail.com>
2022-08-25 01:44:45 -0400

    Add local storage and book display

--- main.js
+++ main.js
@@ -3,20 +3,38 @@
 TODO: Figured out how to push objects into an array, so at this point just need to do the DOM manipulation along with a simple webpage to show the results
 
 TODO 08/22: gathered input from data, DOM manipulation working, made a small popup form (nothing special), as of rn just need to
-            actually display the form data onto the webpage as well as making everything required (check for `` or null or undefined values in input(?));
+actually display the form data onto the webpage as well as making everything required (check for `` or null or undefined values in input(?));
 
 
 TODO 08/23: added a more pleasing looking webpage, figured out how to append child to last value of card-row div but still need to actually display the new book onto the webpage. also still
-            need to toggle read status
+need to toggle read status
+
+
+TODO 08/24: added local storage, everything works however status isnt being updated (if someone were to update it then the update wouldnt display upon refreshing). need to implement remove feature.
 */
 
+document.addEventListener(`DOMContentLoaded`, function () {
+  if (localStorage.getItem(`title`) === null || localStorage.getItem(`title`) === "") {
+    for (let i = 0; i <= parseInt(localStorage.getItem(`incrementer`)); i++) {
+      incrementer = i;
+      localStorage.setItem(`title${i}`, localStorage.getItem(`title${i}`));
+      localStorage.setItem(`author${i}`, localStorage.getItem(`author${i}`));
+      localStorage.setItem(`pageCount${i}`, localStorage.getItem(`pageCount${i}`));
+      localStorage.setItem(`read${i}`, localStorage.getItem(`read${i}`));
+      addBookToWebpage();
+    }
+  }
+}, false);
 
 const newBookButton = document.getElementById(`btnAddForm`);
 const myBooksDiv = document.querySelector(`.myBooks`)
-console.log(myBooksDiv);
+const myButtons = document.querySelectorAll(`.statusButton`)
+
 let myLibrary = [];
+let userBook;
 let clickCount = 0;
 let called = false;
+let incrementer = 0;
 
 
 let title;
@@ -26,6 +44,21 @@ let read;
 
 
 
+function changeStatus() {
+  let statusButtons = document.querySelectorAll(`.statusButton`);
+  statusButtons.forEach(btn => {
+    btn.addEventListener(`click`, function (e) {
+      if (btn.innerText === `Read`) {
+        btn.innerText = `Not yet read`;
+        return;
+      }
+      else {
+        btn.innerText = `Read`;
+        return;
+      }
+    })
+  })
+}
 function Book(title, author, pageCount, read) {
   this.title = title;
   this.author = author;
@@ -36,15 +69,21 @@ function Book(title, author, pageCount, read) {
 
 //function that makes new object and pushes it to myLibrary[]
 function addBookToLibrary(title, author, pageCount, read) {
-
+  localStorage.setItem(`incrementer`, incrementer);
   myLibrary.push({
     title: `${title}`,
     author: `${author}`,
     pageCount: `${pageCount}`,
     read: `${read}`
   });
+  localStorage.setItem(`title${incrementer}`, userBook.title);
+  localStorage.setItem(`author${incrementer}`, userBook.author);
+  localStorage.setItem(`pageCount${incrementer}`, userBook.pageCount);
+  localStorage.setItem(`read${incrementer}`, userBook.read);
 
-  const newBook = new Book(title, author, pageCount, read)
+
+
+
 
 }
 //dynamic form
@@ -131,7 +170,6 @@ function makeForm() {
     const inputBookName = document.getElementById(`title`);
     const inputAuthorName = document.getElementById(`author`);
     const inputPageCount = document.getElementById(`pagecount`);
-    const inputReadBook = document.getElementById(`read`);
 
 
     title = document.querySelector(`#title`).value
@@ -157,8 +195,9 @@ function makeForm() {
       inputPageCount.reportValidity();
       return;
     }
-    let userBook = new Book(title, author, pageCount, read)
+    userBook = new Book(title, author, pageCount, read)
     addBookToLibrary(title, author, pageCount, read);
+
     console.table(userBook);
 
     clickCount = 0;
@@ -186,14 +225,47 @@ function closeForm() {
 }
 
 function addBookToWebpage() {
-
   //creates new H2 Element
-  const newH2Element = document.createElement(`h2`);
-  newH2Element.classList.add(`labelvalue`);
-  newH2Element.setAttribute(`id`,`titlevalue1`)
+  const labelsElement = document.querySelector(`.labels`);
+  const newCardRowElement = document.createElement(`div`);
+  newCardRowElement.classList.add(`card-row`);
+  newCardRowElement.setAttribute(`id`, `book${incrementer}`)
 
-  const cardRow = document.getElementById(`card-row`)
-  cardRow.appendChild(newH2Element)
+  labelsElement.after(newCardRowElement);
+
+  //localStorage.setItem(`title`, userBook.title);
+  //localStorage.setItem(`author`,userBook.author);
+  //localStorage.setItem(`pageCount`,userBook.pageCount);
+  //localStorage.setItem(`read`, userBook.read);
+
+  const newH2TitleElement = document.createElement(`h2`);
+  newH2TitleElement.classList.add(`labelvalue`);
+  newH2TitleElement.setAttribute(`id`, `titlevalue${incrementer}`)
+  newH2TitleElement.innerText = localStorage.getItem(`title${incrementer}`)
+
+  const newH2AuthorElement = document.createElement(`h2`);
+  newH2AuthorElement.classList.add(`labelvalue`);
+  newH2AuthorElement.setAttribute(`id`, `authornamevalue${incrementer}`)
+  newH2AuthorElement.innerText = localStorage.getItem(`author${incrementer}`)
+
+  const newH2PageCountElement = document.createElement(`h2`);
+  newH2PageCountElement.classList.add(`labelvalue`);
+  newH2PageCountElement.setAttribute(`id`, `pagecountvalue${incrementer}`)
+  newH2PageCountElement.innerText = localStorage.getItem(`pageCount${incrementer}`)
+
+  const newButtonStatusElement = document.createElement(`button`);
+  newButtonStatusElement.classList.add(`statusButton`);
+  newButtonStatusElement.setAttribute(`onclick`, `changeStatus()`)
+  newButtonStatusElement.innerText = localStorage.getItem(`read${incrementer}`)
+
+
+  // const cardRow = document.getElementById(`card-row`)
+  newCardRowElement.appendChild(newH2TitleElement)
+  newCardRowElement.appendChild(newH2AuthorElement)
+  newCardRowElement.appendChild(newH2PageCountElement)
+  newCardRowElement.appendChild(newButtonStatusElement)
+
+  incrementer++;
 
   //const lastElement = document.getElementById(`titlevalue`);
   //lastElement.innerText = `Testing this`
@@ -209,7 +281,5 @@ function looper(array) {
   }
   console.log(array);
 }
+//changeStatus();
 //looper(myLibrary);
-
-
-
